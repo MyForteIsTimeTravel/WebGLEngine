39d75b3a7255a211a8a4a148991fed59a74ed733
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-12-10 19:09:30 +0000

    per-face shading

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight
 document.body.appendChild(canvas)
 
 var gl = canvas.getContext('webgl')
-gl.clearColor(0.44, 0.2, 0.14, 1.0)
+gl.clearColor(0.44, 0.2, 0.24, 1.0)
 gl.clear(gl.COLOR_BUFFER_BIT)
 gl.enable(gl.DEPTH_TEST)
 gl.enable(gl.CULL_FACE)
@@ -13,25 +13,34 @@ gl.cullFace(gl.BACK)
 
 var vertexShader = gl.createShader(gl.VERTEX_SHADER)
 gl.shaderSource(vertexShader, [
-    'attribute vec3 position;',
+    'attribute vec3 vertPos;',
+    'attribute vec3 vertCol;',
+    'varying vec3 fragCol;',
     'uniform mat4 model;',
     'uniform mat4 view;',
     'uniform mat4 projection;',
     'void main (void) {',
-        'gl_Position = projection * view * model * vec4(position, 1.0);',
+        'fragCol = vertCol;',
+        'gl_Position = projection * view * model * vec4(vertPos, 1.0);',
     '}'
 ].join('\n'))
 gl.compileShader(vertexShader)
+if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+	console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+}
 
 var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
 gl.shaderSource(fragmentShader, [
     'precision highp float;',
-    'uniform vec4 color;',
+    'varying vec3 fragCol;',
     'void main (void) {',
-        'gl_FragColor = color;',
+        'gl_FragColor = vec4(fragCol, 1.0);',
     '}'
 ].join('\n'))
 gl.compileShader(fragmentShader)
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+	}
 
 var Shader = gl.createProgram()
 gl.attachShader(Shader, vertexShader)
@@ -46,40 +55,40 @@ var triVertices = new Float32Array ([
 
 var boxVertices = new Float32Array ([
         // Top
-		-1.0, 1.0, -1.0,
-		-1.0, 1.0, 1.0,
-		1.0, 1.0, 1.0,
-		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,   0.6, 0.2, 0.2,
+		-1.0, 1.0, 1.0,    0.6, 0.2, 0.2,
+		1.0, 1.0, 1.0,     0.6, 0.2, 0.2,
+		1.0, 1.0, -1.0,    0.6, 0.2, 0.2,
 
 		// Left
-		-1.0, 1.0, 1.0,
-		-1.0, -1.0, 1.0,
-		-1.0, -1.0, -1.0,
-		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,    0.3, 0.1, 0.3,
+		-1.0, -1.0, 1.0,   0.3, 0.1, 0.3,
+		-1.0, -1.0, -1.0,  0.3, 0.1, 0.3,
+		-1.0, 1.0, -1.0,   0.3, 0.1, 0.3,
 
 		// Right
-		1.0, 1.0, 1.0,
-		1.0, -1.0, 1.0, 
-		1.0, -1.0, -1.0,
-		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,     0.3, 0.6, 0.5,
+		1.0, -1.0, 1.0,    0.3, 0.6, 0.5,
+		1.0, -1.0, -1.0,   0.3, 0.6, 0.5,
+		1.0, 1.0, -1.0,    0.3, 0.6, 0.5,
 
 		// Front
-		1.0, 1.0, 1.0, 
-		1.0, -1.0, 1.0,
-		-1.0, -1.0, 1.0,
-		-1.0, 1.0, 1.0, 
+		1.0, 1.0, 1.0,    0.1, 0.2, 0.3,
+		1.0, -1.0, 1.0,   0.1, 0.2, 0.3,
+		-1.0, -1.0, 1.0,  0.1, 0.2, 0.3,
+		-1.0, 1.0, 1.0,   0.1, 0.2, 0.3,
 
 		// Back
-		1.0, 1.0, -1.0,
-		1.0, -1.0, -1.0,
-		-1.0, -1.0, -1.0,
-		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,   0.3, 0.3, 0.2, 
+		1.0, -1.0, -1.0,  0.3, 0.3, 0.2, 
+		-1.0, -1.0, -1.0, 0.3, 0.3, 0.2, 
+		-1.0, 1.0, -1.0,  0.3, 0.3, 0.2, 
 
 		// Bottom
-		-1.0, -1.0, -1.0, 
-		-1.0, -1.0, 1.0,
-		1.0, -1.0, 1.0,
-		1.0, -1.0, -1.0
+		-1.0, -1.0, -1.0, 0.1, 0.3, 0.3, 
+		-1.0, -1.0, 1.0,  0.1, 0.3, 0.3, 
+		1.0, -1.0, 1.0,   0.1, 0.3, 0.3, 
+		1.0, -1.0, -1.0,  0.1, 0.3, 0.3
 ])
 
 var boxIndices = new Uint16Array ([
@@ -117,13 +126,16 @@ gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, EBO)
 gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, boxIndices, gl.STATIC_DRAW)
 
 // upload static shader data
-gl.useProgram(Shader)
-Shader.color = gl.getUniformLocation(Shader, 'color')
-gl.uniform4fv(Shader.color, [0.6, 0.2, 0.2, 1.0])
+var posAttribLoc = gl.getAttribLocation(Shader, 'vertPos')
+var colAttribLoc = gl.getAttribLocation(Shader, 'vertCol')
 
-Shader.position = gl.getAttribLocation(Shader, 'position')
-gl.enableVertexAttribArray(Shader.position)
-gl.vertexAttribPointer(Shader.position, 3, gl.FLOAT, false, 0, 0)
+gl.vertexAttribPointer(posAttribLoc, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0)
+gl.vertexAttribPointer(colAttribLoc, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT)
+
+gl.enableVertexAttribArray(posAttribLoc);
+gl.enableVertexAttribArray(colAttribLoc);
+
+gl.useProgram(Shader)
 
 // view transform
 var view  = new Float32Array(16)
